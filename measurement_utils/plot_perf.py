b8f30abcb6669c11ad615fd9869661c040442f9d
Stephanie Ruhstaller <ruhstals@student.ethz.ch>
2022-05-01 18:33:25 +0200

    beautify plot

--- measurement_utils/plot_perf.py
+++ measurement_utils/plot_perf.py
@@ -4,8 +4,21 @@
 
 import os
 from re import X
+from pathlib import Path
 import pandas as pd
+from pytest import mark
+import seaborn as sns
+import matplotlib
 import matplotlib.pyplot as plt
+from labellines import labelLines
+import itertools
+
+def cycle_markers(iterable,n):
+  for item in itertools.cycle(iterable):
+    yield item
+
+sns.set_theme()
+markers = cycle_markers(('^','o','s'),1)
 
 PATH = './'
 
@@ -17,13 +30,21 @@ for file in fileNames:
     df = pd.read_csv(PATH + file, usecols= [0,1,2])
     x = df.iloc[:,0].to_numpy()
     y = df.iloc[:,2].to_numpy()/df.iloc[:,1].to_numpy()
-    plt.plot(x, y, label = f"{file}")
+    marker = next(markers)
+    plt.plot(x, y, label = f"{Path(file).stem}", marker=marker)
 
+plt.title("Belief Propagation [Processor, Flags ...]")
 plt.xlabel('n')
 plt.ylabel('Performance [F/C]')
-plt.grid()
-plt.legend()
+#plt.legend()
 plt.ylim(ymin=0,ymax=2)
+plt.ylabel('flops/cycle')
+#plt.grid()
+ax = plt.gca()
+ax.get_xaxis().set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+ax.grid(b=True, which='major', color='w', linewidth=1.0)
+ax.grid(b=True, which='minor', color='w', linewidth=0.5)
+labelLines(ax.get_lines(), yoffsets=0.1)
 ### Generate the plot
 plt.savefig('./plots/performance_plot.png')  
 plt.show() 
