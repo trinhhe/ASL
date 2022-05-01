f3762016851ca9b7b52e08191dcdb18457146488
Stephanie Ruhstaller <ruhstals@student.ethz.ch>
2022-05-01 17:41:06 +0200

    simplified bash script

--- measurement_utils/measure_runtime.sh
+++ measurement_utils/measure_runtime.sh
@@ -1,21 +1,9 @@
 #!/bin/sh
-#from_scatch
-filename='from_scratch_O3.csv'
-# gcc ../from_scratch/src/main_timer.c -o ../from_scratch/src/main_timer_O3 -g -Wall -lm -O3
-echo "n (number of vertices), total_cycle, total_flops, gbuild_cycle, prop_cycle, bel_cycle, gbuild_flops, prop_flops, bel_flops" > $filename
-./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_10user_norm.csv >> $filename
-./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_20user_norm.csv >> $filename
-./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_30user_norm.csv >> $filename
-./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_60user_norm.csv >> $filename
-./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_90user_norm.csv >> $filename
-./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_120user_norm.csv >> $filename
-./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_150user_norm.csv >> $filename
-./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_210user_norm.csv >> $filename
-./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_270user_norm.csv >> $filename
-./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_330user_norm.csv >> $filename
-./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_390user_norm.csv >> $filename
-./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_450user_norm.csv >> $filename
-./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_540user_norm.csv >> $filename
-./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_all_norm.csv >> $filename
-
-
+# $1: first argument, additional compiler flags e.g. '-O3 -ffast-math'
+# $2: second argument, output file name
+gcc ../from_scratch/src/main_timer.c -o main_timer -g -Wall -lm "$1"
+echo "n (number of vertices), total_cycle, total_flops, gbuild_cycle, prop_cycle, bel_cycle, gbuild_flops, prop_flops, bel_flops" > "$2"
+for testfile in ../from_scratch/test/*norm.csv; do
+    ./main_timer "$testfile" >> "$2" 
+done
+rm main_timer
