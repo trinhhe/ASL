e47fd8fb4d1609563f41ec081fa55888c32c2300
trinhhe <henry.trinh94@gmail.com>
2022-05-01 15:06:53 +0200

    add free memories in form_scratch/main_timer.c + flop count without bp2.run in libdai_impl yet

--- measurement_utils/measure_runtime.sh
+++ measurement_utils/measure_runtime.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #from_scatch
-filename='from_scatch_performance.csv'
+filename='from_scratch_O3.csv'
+# gcc ../from_scratch/src/main_timer.c -o ../from_scratch/src/main_timer_O3 -g -Wall -lm -O3
 echo "n (number of vertices), total_cycle, total_flops, gbuild_cycle, prop_cycle, bel_cycle, gbuild_flops, prop_flops, bel_flops" > $filename
 ./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_10user_norm.csv >> $filename
 ./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_20user_norm.csv >> $filename
@@ -16,4 +17,5 @@ echo "n (number of vertices), total_cycle, total_flops, gbuild_cycle, prop_cycle
 ./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_450user_norm.csv >> $filename
 ./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_540user_norm.csv >> $filename
 ./../from_scratch/src/main_timer_O3 ../from_scratch/test/ratings_all_norm.csv >> $filename
+
 
