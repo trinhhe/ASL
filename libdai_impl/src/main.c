767a59e72268c7905941f0aa1751a6680869682b
Stephanie Ruhstaller <ruhstals@student.ethz.ch>
2022-05-02 12:34:32 +0200

    changed structure for library implementation

---
+++ libdai_impl/src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "recommender.h"
+
+// First build the library yourself as described in the README in the libdai folder
+// Switch to folder: cd libdai_impl
+// Compile with: make
+int main(int argc, char *argv[]) {
+
+    const char* fileLocation = argc > 1 ? argv[1] : "test/data/ratings_small.csv";
+    const int targetUser = argc > 2 ? atoi(argv[2]) : 1; // TODO: check inputs
+    const size_t k = argc > 3 ? atoi(argv[3]) : 0;
+
+    rinput_t ri;
+    initialize(&ri, fileLocation,targetUser);
+    int* recommendedK = recommendK(&ri, k);
+
+    printf("Recommended movies: \n");
+    for(size_t i; i<k; i++){
+        printf("%i ", recommendedK[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
