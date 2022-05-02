593ecfb3ae424e41bfdb46ae62be3a85ff355bbc
Stephanie Ruhstaller <ruhstals@student.ethz.ch>
2022-05-02 08:13:18 +0200

    moved measurement test data

--- measurement_utils/measure_runtime.sh
+++ measurement_utils/measure_runtime.sh
@@ -4,7 +4,7 @@
 # $2: second argument, output file name
 gcc ../from_scratch/src/main_timer.c -o main_timer -g -Wall -lm "$1"
 echo "n (number of vertices), total_cycle, total_flops, gbuild_cycle, prop_cycle, bel_cycle, gbuild_flops, prop_flops, bel_flops" > "$2"
-for testfile in ../from_scratch/test/*norm.csv; do
+for testfile in data/*norm.csv; do
     ./main_timer "$testfile" >> "$2" 
 done
 rm main_timer
