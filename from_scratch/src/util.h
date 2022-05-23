b1f2c8653e3cf2bd4a98ca9f23a581cf13573145
Richard Hladík <rihl@uralyx.cz>
2022-05-23 02:19:13 +0200

    Pave way for padded and aligned graph representation

--- from_scratch/src/util.h
+++ from_scratch/src/util.h
@@ -3,6 +3,22 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "constants.h"
+
+void *aligned_malloc(size_t size)
+{
+	void *p = nullptr;
+	posix_memalign(&p, SIMD_ALIGN_BYTES, size);
+	return p;
+}
+
+void *aligned_calloc(size_t nmemb, size_t size) {
+	size_t n = nmemb * size;
+	void *p = aligned_malloc(n);
+	memset(p, 0, n);
+	return p;
+}
 
 // Prints error message and exits
 static void die(const char fmt[], ...)
@@ -15,4 +31,10 @@ static void die(const char fmt[], ...)
 }
 
 static int max(int a, int b) { return a > b ? a : b; }
+
+// assumes multiple is a power of two
+unsigned round_up(unsigned x, unsigned multiple) {
+	auto ret = ((x - 1) & ~(multiple - 1)) + multiple;
+	return ret;
+}
 #endif
