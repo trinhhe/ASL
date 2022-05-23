1133721b0825a6c01ef044bc701dfb95f6d0e2fb
Richard Hladík <rihl@uralyx.cz>
2022-05-23 02:16:47 +0200

    Tweak BP constants

--- from_scratch/src/constants.h
+++ from_scratch/src/constants.h
@@ -2,11 +2,16 @@
 #define BELIEF_CONSTANTS_H
 #include "rating.h"
 const float_t NORMALISE_P = 3;
-const float_t ALPHA = 0.001; // TODO: lower, currently even with such a large ALPHA it does nothing
+const float_t ALPHA = .3;
 const float_t PROP_EQUAL = (float_t)0.5 + ALPHA;
 const float_t PROP_UNEQUAL = (float_t)0.5 - ALPHA;
+const float_t PROP_00 = .8;
+const float_t PROP_01 = .2;
+const float_t PROP_10 = .2;
+const float_t PROP_11 = .8;
 const float_t PROP_MATRIX[2][2] = {
-	{PROP_EQUAL, PROP_UNEQUAL},
-	{PROP_UNEQUAL, PROP_EQUAL}
+	{PROP_00, PROP_01},
+	{PROP_10, PROP_11}
 };
+const size_t SIMD_ALIGN_BYTES = 32;
 #endif
