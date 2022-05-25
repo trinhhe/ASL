fcdcb246ebf81ea82c6304c3efc5225c74c12cd7
Richard Hladík <rihl@uralyx.cz>
2022-05-25 04:06:33 +0200

    Small tweaks

--- from_scratch/src/constants.h
+++ from_scratch/src/constants.h
@@ -6,10 +6,10 @@ const float_t NORMALISE_P = 3;
 const float_t ALPHA = .3;
 const float_t PROP_EQUAL = (float_t)0.5 + ALPHA;
 const float_t PROP_UNEQUAL = (float_t)0.5 - ALPHA;
-const float_t PROP_00 = .8;
-const float_t PROP_01 = .2;
-const float_t PROP_10 = .2;
-const float_t PROP_11 = .8;
+const float_t PROP_00 = PROP_EQUAL;
+const float_t PROP_01 = PROP_UNEQUAL;
+const float_t PROP_10 = PROP_UNEQUAL;
+const float_t PROP_11 = PROP_EQUAL;
 const float_t PROP_MATRIX[2][2] = {
 	{PROP_00, PROP_01},
 	{PROP_10, PROP_11}
