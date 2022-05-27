a0c092fdd33c0498fb63a47dc1824bbdf87a3121
trinhhe <henry.trinh94@gmail.com>
2022-05-27 22:17:45 +0200

    datafolder as arg for generate_subsets.py
    Former-commit-id: f2a4ec51091a904a9eb47f3b72e870e62cf4d7dc

--- measurement_utils/generate_subsets.py
+++ measurement_utils/generate_subsets.py
@@ -7,23 +7,29 @@ import shutil
 
 # user_steps = 3240 # ~162000 users / 3240 = 50 rating files
 user_steps = 16200 # 10 files
+user_steps = 100
 
 def main():   
-    print("Start Process\n")
-    file = open("data/ratings_all.csv", 'r').readlines()
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} folderpath relativ to generate_subsets.py")
+        sys.exit(1)
+    data_folder = sys.argv[1]
+
+    print("Start Generating\n")
+    file = open(data_folder + "/ratings_all.csv", 'r').readlines()
     users = 0
     length = len(file)
     for i in range(length):
         if i!= 0 and int(file[i].split(',')[0]) != users:
             users+=1
             if(users % user_steps == 0):
-                to_write = open("data/ratings_" + str(users) +"user.csv", 'w')
+                to_write = open(data_folder + "/ratings_" + str(users) +"user.csv", 'w')
                 to_write.writelines(file[0:i])
 
     
 
     # shutil.copyfile(file, "data/xd.csv")
-    print("End Process\n")
+    print("End Generating\n")
 
 if __name__ == "__main__":
     main()
--- measurement_utils/get_data.sh
+++ measurement_utils/get_data.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 ### downloads file ###
-# file="ml-latest-small.zip"
-file="ml-25m.zip"
+file="ml-latest-small.zip"
+# file="ml-25m.zip"
 dataURL="https://files.grouplens.org/datasets/movielens/$file"
 
 # DIR="$(dirname "${BASH_SOURCE[0]}")"
 
+dataFolder="data_big"
+
 test -f ../Data/$file || wget -O ../Data/$file $dataURL
-test -f data/ratings_all.csv || (unzip ../Data/$file -d ../Data/ && mv ../Data/${file%.*}/ratings.csv data/ratings_all.csv && rm -r ../Data/${file%.*})
+test -f $dataFolder/ratings_all.csv || (unzip ../Data/$file -d ../Data/ && mv ../Data/${file%.*}/ratings.csv $dataFolder/ratings_all.csv && rm -r ../Data/${file%.*})
 
-test -f data/ratings_all_norm.csv || (python normcsv.py data/ratings_all.csv data/ratings_all_norm.csv)
+test -f $dataFolder/ratings_all_norm.csv || (python normcsv.py $dataFolder/ratings_all.csv $dataFolder/ratings_all_norm.csv)
 
-python generate_subsets.py
+python generate_subsets.py "$dataFolder"
 
-for csvfile in data/*user.csv
+for csvfile in $dataFolder/*user.csv
 do
     python normcsv.py "$csvfile" "${csvfile%.csv}_norm.csv"
 done
