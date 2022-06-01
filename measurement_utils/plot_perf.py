0aee899049a15da60caad59ad1396e1d3dfdf269
Stephanie Ruhstaller <ruhstals@student.ethz.ch>
2022-06-01 12:35:46 +0200

    added runtime plot

--- measurement_utils/plot_perf.py
+++ measurement_utils/plot_perf.py
@@ -7,6 +7,7 @@ import os
 import glob
 from re import X
 from pathlib import Path
+from jinja2 import PrefixLoader
 import pandas as pd
 from pytest import mark
 import seaborn as sns
@@ -15,6 +16,8 @@ import matplotlib.pyplot as plt
 from labellines import labelLines
 import itertools
 import sys
+
+from sympy import Q
 
 show = True
 if len(sys.argv) > 1 and sys.argv[1] == "-n":
@@ -33,44 +36,97 @@ OUT = PATH + '/plots/'
 fileNames = glob.glob(f"{PATH}/*.csv")
 fileNames = sorted(fileNames)
 
+perfFigure = plt.figure(1)
+ax1 = perfFigure.gca()
+rtFigure = plt.figure(2)
+ax2 = rtFigure.gca()
+cyclesVsFlopsFigure = plt.figure(3)
+ax3 = cyclesVsFlopsFigure.gca()
+
 for file in fileNames:
     df = pd.read_csv(file, usecols= [0,1,2], index_col=False)
-    x = df.iloc[:,0].to_numpy()
-    y = df.iloc[:,2].to_numpy()/df.iloc[:,1].to_numpy()
+    input_sizes = df.iloc[:,0].to_numpy()
+    total_flops = df.iloc[:,2].to_numpy()
+    total_cycles = df.iloc[:,1].to_numpy()
+    flops_per_cycle = total_flops / total_cycles
     #sort array after n
-    perm = x.argsort()
-    y = y[perm]
-    x = x[perm]
-    if not x.size:
+    perm = input_sizes.argsort()
+    input_sizes = input_sizes[perm]  
+    total_flops = total_flops[perm]
+    total_cycles = total_cycles[perm]
+    flops_per_cycle = flops_per_cycle[perm]
+    if not input_sizes.size:
         # the corresponding run has probably crashed, if we don't ignore it, we run into problems later
         continue
     marker = next(markers)
     pretty_name = Path(file).stem.replace("@", " ").replace("__", "=")
-    plt.plot(x, y, label = pretty_name, marker=marker)
+    ax1.plot(input_sizes, flops_per_cycle, label = pretty_name, marker=marker)
+    ax2.plot(input_sizes, total_cycles, label = pretty_name, marker=marker)
+    ax3.plot(total_cycles, total_flops, label = pretty_name, marker=marker)
 
-plt.title("Belief Propagation [Processor, Flags ...]")
-plt.xlabel('n')
-plt.ylabel('Performance [F/C]')
-plt.ylim(ymin=0)
-plt.ylabel('flops/cycle')
-#plt.grid()
-ax = plt.gca()
-ax.get_xaxis().set_minor_locator(matplotlib.ticker.AutoMinorLocator())
-ax.grid(b=True, which='major', color='w', linewidth=1.0)
-ax.grid(b=True, which='minor', color='w', linewidth=0.5)
-#labelLines(ax.get_lines(), 
+ax1.set_title("Belief Propagation [Processor, Flags ...]")
+ax1.set_xlabel('n')
+ax1.set_ylim(ymin=0)
+ax1.set_ylabel('flops/cycle')
+#ax1.grid()
+ax1.get_xaxis().set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+ax1.grid(visible=True, which='major', color='w', linewidth=1.0)
+ax1.grid(visible=True, which='minor', color='w', linewidth=0.5)
+#labelLines(ax1.get_lines(), 
 #            yoffsets=0.022, 
 #            fontsize=10, 
 #            align=False, 
 #            outline_color=None, 
 #            outline_width=0)
-box = ax.get_position()
-ax.set_position([box.x0, box.y0 + box.height * 0.3,
+box = ax1.get_position()
+ax1.set_position([box.x0, box.y0 + box.height * 0.3,
                  box.width, box.height * 0.7])
-ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=1,prop={'size': 6})
+ax1.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=1,prop={'size': 6})
 ### Generate the plot
-plt.savefig(OUT + 'performance_plot.png')
-plt.savefig(OUT + 'performance_plot.pdf')
+perfFigure.savefig(OUT + 'performance_plot.png')
+
+ax2.set_title("Belief Propagation [Processor, Flags ...]")
+ax2.set_xlabel('n')
+ax2.set_ylim(ymin=0)
+ax2.set_ylabel('cycles')
+#ax2.grid()
+ax2.get_xaxis().set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+ax2.grid(visible=True, which='major', color='w', linewidth=1.0)
+ax2.grid(visible=True, which='minor', color='w', linewidth=0.5)
+#labelLines(ax2.get_lines(), 
+#            yoffsets=0.022, 
+#            fontsize=10, 
+#            align=False, 
+#            outline_color=None, 
+#            outline_width=0)
+box = ax2.get_position()
+ax2.set_position([box.x0, box.y0 + box.height * 0.3,
+                 box.width, box.height * 0.7])
+ax2.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=1,prop={'size': 6})
+### Generate the plot
+rtFigure.savefig(OUT + 'runtime_plot.png')
+
+ax3.set_title("Belief Propagation [Processor, Flags ...]")
+ax3.set_xlabel('cycles')
+ax3.set_ylim(ymin=0)
+ax3.set_ylabel('flops')
+#ax3.grid()
+ax3.get_xaxis().set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+ax3.grid(visible=True, which='major', color='w', linewidth=1.0)
+ax3.grid(visible=True, which='minor', color='w', linewidth=0.5)
+#labelLines(ax3.get_lines(), 
+#            yoffsets=0.022, 
+#            fontsize=10, 
+#            align=False, 
+#            outline_color=None, 
+#            outline_width=0)
+box = ax3.get_position()
+ax3.set_position([box.x0, box.y0 + box.height * 0.3,
+                 box.width, box.height * 0.7])
+ax3.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=1,prop={'size': 6})
+### Generate the plot
+cyclesVsFlopsFigure.savefig(OUT + 'cyclesVsFlops_plot.png')
+
 if show:
     plt.show()
 plt.close()
